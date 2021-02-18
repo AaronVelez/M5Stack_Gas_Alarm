cdff06040ab8b9bd6185da10581331de40526614
Aarón I. Vélez Ramírez <aaron.velez@comunidad.unam.mx>
2021-02-18 12:15:17 -0600

    Visual-Micro solution created

---
+++ Arduino-code/Arduino-code.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino_code.ino
+ Created:	2/18/2021 12:12:37 PM
+ Author:	aivel
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
